77c5c6a01313700bb9fa626bca03d9960f7e456b
charrier enzo <charrier@lan.sio.lyceefulbert.fr>
2026-02-06 15:47:46 +0100

    Debut code

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>		
+    <html lang= "fr">
+
+        <head>
+	        <meta charset= "utf-8">	
+	        <title>Labyrinthe</title>
+            <link rel="stylesheet" href="style/style.css">
+        </head>
+        <body>
+
+            <?php
+ 	
+                echo "<header>";
+                echo "<h1> Test </h1>";
+            ?>
+        </body>
+    </html>
