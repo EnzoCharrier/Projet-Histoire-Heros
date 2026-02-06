294cd2cb64206adfad0baaa64dd95c79bfc9fbea
charrier enzo <charrier@lan.sio.lyceefulbert.fr>
2026-02-06 16:55:26 +0100

    Debut code

--- src/index.php
+++ src/index.php
@@ -1,5 +1,24 @@
 <?php
+	
+	// Database settings
+	$db="HistoireHeros";
+	$dbhost="192.168.56.10";
+	$dbport=3306;
+	$dbuser="admin";
+	$dbpasswd="admin";
  
+	$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+	$pdo->exec("SET CHARACTER SET utf8");
+
+	$req='SELECT * FROM Choix WHERE id_histoire =:id';
+    $requete = $pdo -> prepare($req);	
+	$requete -> bindValue(':id', 2, PDO::PARAM_INT);
+	$requete -> execute();
+	$resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
+	echo '<pre>';
+		print_r($resultats);
+	echo '</pre>';
+
 	include 'Controleur/controleur.php';
 
 	if(isset($_GET['page'])) $page = $_GET['page'];
@@ -10,5 +29,5 @@
 		$monAccueil = new ControleurAccueil();
 		$monAccueil -> afficherAccueil();
 	}
-
+$pdo = null;
 ?>
