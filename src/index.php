7f80b79820f02d34690455e39627deff3ed77c0d
charrier enzo <charrier@lan.sio.lyceefulbert.fr>
2026-03-06 15:14:00 +0100

    Ajout connexion BD

--- src/index.php
+++ src/index.php
@@ -1,33 +1,17 @@
 <?php
-	
-	// Database settings
-	$db="HistoireHeros";
-	$dbhost="192.168.56.10";
-	$dbport=3306;
-	$dbuser="admin";
-	$dbpasswd="admin";
- 
-	$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
-	$pdo->exec("SET CHARACTER SET utf8");
+// Connexion à la BD
+$db="HistoireHeros";
+$dbhost="192.168.56.10";
+$dbport=3306;
+$dbuser="admin";
+$dbpasswd="admin";
 
-	$req='SELECT * FROM Choix WHERE id_histoire =:id';
-    $requete = $pdo -> prepare($req);	
-	$requete -> bindValue(':id', 2, PDO::PARAM_INT);
-	$requete -> execute();
-	$resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
-	echo '<pre>';
-		print_r($resultats);
-	echo '</pre>';
+$pdo = new PDO("mysql:host=$dbhost;port=$dbport;dbname=$db", $dbuser, $dbpasswd);
+$pdo->exec("SET NAMES utf8");
 
-	include 'Controleur/controleur.php';
+// Appel du contrôleur
+require_once("Controleur/ControleurHistoire.php");
 
-	if(isset($_GET['page'])) $page = $_GET['page'];
-	else $page = 1;
-
-	
-	if($page == 1){
-		$monAccueil = new ControleurAccueil();
-		$monAccueil -> afficherAccueil();
-	}
-$pdo = null;
+$controleur = new ControleurHistoire($pdo);
+$controleur->afficher();
 ?>
