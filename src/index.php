a106835e1a87c284fa00b67e8a92cd632e0620d4
charrier enzo <charrier@lan.sio.lyceefulbert.fr>
2026-04-10 16:43:27 +0200

    modif bdd

--- src/index.php
+++ src/index.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 // Connexion à la BD
-//$db="HistoireHeros";
-//$dbhost="192.168.56.10";
-//$dbport=3306;
-//$dbuser="admin";
-//$dbpasswd="admin";
-
-$db="charrier";
-$dbhost="charrier.slam.lab";
+$db="HistoireHeros";
+$dbhost="192.168.56.10";
 $dbport=3306;
-$dbuser="charrier";
-$dbpasswd="QiDjr9mq5PEF";
+$dbuser="admin";
+$dbpasswd="admin";
+
+//$db="charrier";
+//$dbhost="charrier.slam.lab";
+//$dbport=3306;
+//$dbuser="charrier";
+//$dbpasswd="QiDjr9mq5PEF";
 
 
 $pdo = new PDO("mysql:host=$dbhost;port=$dbport;dbname=$db", $dbuser, $dbpasswd);
